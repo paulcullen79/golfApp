c29763bb4e526e081511bf720eaa3d823a520893
paulcullen79 <cullenpaul79@gmail.com>
2019-12-07 22:52:09 -0500

    runs locally only

--- server.js
+++ server.js
@@ -3,7 +3,11 @@
 // function imported from teams-data.js
 const getTeamsData = require('./teams-data')
 
-const teamNames = ['Pauls-Balls', 'Tipperary Stonethrowers', 'Premier County', 'Jordan Spieth - simply the best!', 'The Clare Savages']
+const teamNames = [ 'Pauls-Balls', 
+                    'Tipperary Stonethrowers', 
+                    'Premier County', 
+                    'Jordan Spieth - simply the best!', 
+                    'The Clare Savages']
 const data = getTeamsData(teamNames)
 
 const express = require('express') 
@@ -14,9 +18,9 @@ app.use(cors())
 
 app.get('/golfpool-standings', (req, res) => {
     res.send(data)
+    console.log('serverTest')
     
 })
 
 const port = process.env.port || 3000
 app.listen(port, () => console.log(`CORS enabled: Listening on port ${port}...`))
-
